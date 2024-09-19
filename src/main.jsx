78cbcbe347b36912be0653a35246a4360665dc4e
NicoWijaya0355 <90439261+NicoWijaya0355@users.noreply.github.com>
2024-09-19 12:11:22 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import styles from "./App.module.css";
-import { About } from "./components/About/About";
-import { Contact } from "./components/Contact/Contact";
-import { Experience } from "./components/Experience/Experience";
-import { Hero } from "./components/Hero/Hero";
-
-import { Navbar } from "./components/Navbar";
-import { Projects } from "./components/Projects/Projects";
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className={styles.App}>
-      <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-    </div>
-    
+   <App />
   </StrictMode>,
 );
 
