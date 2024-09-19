ddbedb24a114d4daadb35cbd8a2cdd738bd2593e
NicoWijaya0355 <90439261+NicoWijaya0355@users.noreply.github.com>
2024-09-19 12:07:17 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,18 +9,8 @@ export const Navbar =() => {
      <nav className={styles.navbar}>
           <a className={styles.title} href="/">Portfolio</a>
           <div className={styles.menu}>
-               {/* <img 
-               className={styles.menuBtn} 
-               src={
-                    menuOpen 
-                    ? getImageUrl("nav/closeIcon.png")
-                    : getImageUrl("nav/menuIcon.png")
-               } 
-               alt="menu-button"
-               onClick={() => setMenuOpen(!menuOpen)}
-               /> */}
-               <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-               onClick={() => setMenuOpen(false)}>
+               
+               <ul>
                     <li>
                          <a href="#about">About</a>
                     </li>
