05d4c1a06ef7bd9a71cd78ae7969686c412f3f23
NicoWijaya0355 <90439261+NicoWijaya0355@users.noreply.github.com>
2024-09-19 11:58:24 +0700

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,17 +9,20 @@ export const Contact = () => {
         </div>
         <ul className={styles.links}>
             <li className={styles.link}>
-                <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon "/>
-                <a href="mailto:nicowijaya56@gmail.com">nicowijaya56@gmail.com</a>
+                <a href="mailto:nicowijaya56@gmail.com">
+                <img href="mailto:nicowijaya56@gmail.com" src={getImageUrl("contact/emailIcon.png")} alt="Email icon "/></a>
+                <a className={styles.Contact} href="mailto:nicowijaya56@gmail.com">nicowijaya56@gmail.com</a>
             </li>
             <li className={styles.link}>
-
-                <img src={getImageUrl("contact/linkedinIcon.png")} alt="Email icon "/>
-                <a href="https://www.linkedin.com/in/nico-wijaya-bb6104226/">Nico Wijaya</a>
+                <a href="https://www.linkedin.com/in/nico-wijaya-bb6104226/">
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="Email icon "/></a>
+                <a className={styles.Contact} href="https://www.linkedin.com/in/nico-wijaya-bb6104226/">Nico Wijaya</a>
             </li>
             <li className={styles.link}>
-                <img src={getImageUrl("contact/wa.png")} alt="WA icon "/>
-                <a href="https://api.whatsapp.com/send?phone=08977164040">08977164040</a>
+                <a href="https://api.whatsapp.com/send?phone=08977164040">
+                <img src={getImageUrl("contact/wa.png")} alt="WA icon "/></a>
+                <a className={styles.Contact} href="https://api.whatsapp.com/send?phone=08977164040">08977164040</a>
+                
             </li>
         </ul>
     </footer>;
