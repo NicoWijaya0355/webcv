9bfe1287174af81f93e1a0bcb04cc62e7faf8a35
NicoWijaya0355 <90439261+NicoWijaya0355@users.noreply.github.com>
2024-10-15 11:08:32 +0700

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () =>{
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Nico</h1>
-            <p className={styles.description}>Graduated as bachelor's degree in Informatics. Has a strong interest in web development </p>
+            <p className={styles.description}>A motivated and detail-oriented Informatics graduate with a strong foundation in web development and programming. Experienced in using HTML, CSS, Laravel, Vue.js, and SQL, I have successfully completed several projects that demonstrate my technical skills and ability to work collaboratively in a team environment. As a quick learner, I am eager to expand my knowledge in programming and other related fields, adapting to new challenges and technologies with enthusiasm. I am committed to continuous improvement and contributing effectively to team success. </p>
             <a href="mailto:nicowijaya56@gmail.com" className={styles.contactBtn}>Contact Me</a>
             </div>
             <img src={getImageUrl("hero/berwarna.png")} alt="Hero image of me" className={styles.heroImg}></img>
